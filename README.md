6cc73c49eade44b6ce2db17c0146fc6b6ac4ce5b
Dov DEVERS <dov.devers@gmail.com>
2020-10-01 18:10:26 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,37 @@
+# CheckStudents
+
+## Fonctionnement
+
+
+Pour voir les élèves présents à un cours, un professeur (ou toute autre utilisateur ayant un role autorisé voir PARTIE ~)
+
+>Pour faire l'appel d'une classe, il suffit de d'envoyer `.Check appel @Classe` (remplacer `@classe` par le role correspondant)
+<img src="img/img1.png" alt="Capture d'écran d'une recherche">
+
+>Des émojis vont apparaitre en dessous de votre message, les élèves faisant partie de la `@classe` pourront cliquer sur le ✅ pour notifier leur présence.
+Le professeur, ou n'importe quelle personne ayant les droits pourra finir l'appel en cliquant sur 🆗.
+<img src="img/img2.png" alt="Capture d'écran d'une recherche">
+
+>Note : Les utilisateurs ne peuvent notifier leur présence que si ils font partie du groupe (les élèves extérieurs ne seront pas comptabilisés.)
+>De même, quelqu'un qui n'a pas les privilèges ne peut fermet l'appel
+<img src="img/img3.png" alt="Capture d'écran d'une recherche">
+
+
+Une fois l'appel terminé, le bit retournera la liste des élèves présents (sortira les élèves non présent dans une MAJ).
+<img src="img/img4.png" alt="Capture d'écran d'une recherche">
+
+## Initialisation
+
+Pour initialiser le bot, vous devez écrire `.Check init @roleBot @admin` -> @roleBot correspond au role du bot CheckStudents et @admin le rôle de privilège.
+<img src="img/img5.png" alt="Capture d'écran d'une recherche">
+
+## Ajouter/Supprimer des privilèges à un rôle
+
+Seul un utilisateur ayant les privilèges peut rajouter/supprimer des rôles.
+
+* Ajouter : `.Check addRole @role1 @role2,...` 
+* Supprimer : `.Check rmRole @role1 @role2,...`
+>Note : Vous pouvez rajouter/supprimer autant de role que vous vouler à la fois.
+
+
+Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
---
+++ app.py
@@ -0,0 +1,168 @@
+import discord
+import json
+from discord.ext import commands
+# from CONSTANT import *
+import sys
+# import time
+commandes="""
+.Check  appel ***@classe***-> lance l'appel, **remplacer par la classe correspondante**
+.Check addRole @role1 @role2,... -> rajoute les privilèges à un ou plusieurs rôles
+.Check rmRole  @role1 @role2,... -> retire les privilèges à un ou plusieurs rôles
+.Check init  @roleofTheBot -> initialise le bot pour la première sur un serveur : **Préciser le rôle du bot !**
+"""
+
+
+def jsonWrite(path,data):
+    with open(path, 'w') as outfile:
+        outfile.write(data)
+
+client = commands.Bot(command_prefix= '.Check ')
+
+
+token=sys.argv[1]
+
+liste_eleves=dict()
+def isInit(guild):
+    return str(guild.id) in liste_eleves.keys()
+
+def got_the_role(role,authorRoles:list):
+    if isinstance(role,list):
+        for i in role:
+            if i in [y.id for y in authorRoles]:
+                return True
+    elif isinstance(role,int):
+        return role in [y.id for y in authorRoles]
+
+def returnPresent(message):
+    print(liste_eleves)
+    liste=liste_eleves[str(message.guild.id)]['appels'][str(message.id)]['listStudents']
+    if liste==[]:
+        return "⚠ **Aucun élève présent** ⚠"
+    else:
+        message="**Liste des élèves présents :**\n"
+        for i in liste:
+            message+="• *{}* <@{}>\n".format(i[0],i[1])  #[user.display_name,user.id]
+        return message
+def convert(id):
+    return int(id.replace(" ","").lstrip("<@&").rstrip(">"))
+
+async def on_ready():
+    """Initilisation du bot"""
+    print('Bot started')
+
+@client.command()
+async def send(message,channel):
+    await channel.send(message)
+
+@client.command()
+async def add_reaction(emoji,message):
+    await message.add_reaction(emoji)
+
+@client.command()
+async def remove_reaction(emoji,message,user):
+    await message.remove_reaction(emoji,user)
+
+@client.command()
+async def clear_reaction(emoji,message):
+    await message.clear_reaction(emoji)
+
+@client.event
+async def on_reaction_add(reaction, user):
+    global liste_eleves
+    idMessage=str(reaction.message.id)
+    idGuild=str(reaction.message.guild.id)
+
+    if idMessage in liste_eleves[idGuild]['appels']: #si c'est un message d'appel lancé par un professeur
+        reactionContent=str(reaction).strip(" ")
+
+        if reactionContent=="✅": #si l'utilisateur a coché présent
+            if  got_the_role(liste_eleves[idGuild]['appels'][idMessage]['ClasseRoleID'],user.roles): #si user a le role de la classe correspondante
+                liste_eleves[idGuild]['appels'][idMessage]['listStudents'].append([user.display_name,user.id]) #on le rajoute à la liste d'appel
+
+            elif not got_the_role(liste_eleves[idGuild]['botID'],user.roles):
+                await remove_reaction("✅",reaction.message,user)
+                await send("<@{}> : Vous ne pouvez pas vous notifier présent".format(user.id,liste_eleves[idGuild]['appels'][idMessage]['ClasseRoleID']),reaction.message.channel)
+
+
+        elif reactionContent=="🆗": #si l'utilisateur a coché OK
+            if got_the_role(liste_eleves[idGuild]['admin'],user.roles): # est prof
+
+                await send("<@{}> : A fini l'appel des <@&{}>".format(user.id,liste_eleves[idGuild]['appels'][idMessage]['ClasseRoleID']),reaction.message.channel)
+                await clear_reaction("✅",reaction.message)
+                await clear_reaction("🆗",reaction.message)
+                await send(returnPresent(reaction.message),reaction.message.channel)
+                del liste_eleves[idGuild]['appels'][idMessage]
+
+            elif not got_the_role(liste_eleves[idGuild]['botID'],user.roles): #pas le bot
+                await remove_reaction("🆗",reaction.message,user)
+                await send("<@{}> : **Vous n'avez pas les droits pour fermer l'appel !**".format(user.id),reaction.message.channel)
+
+        else: # autre emoji
+            await remove_reaction(reactionContent,reaction.message,user)
+            await send("<@{}> : **Emoji inconnu.\nLes élèves doivent cliquer sur ✅ pour se notifier présent.**".format(user.id),reaction.message.channel)
+
+
+@client.command()
+async def appel(context,*args):
+    global liste_eleves
+    if len(args)>1:
+        raise commands.errors.CommandNotFound
+    if not str(context.guild.id) in liste_eleves.keys():
+        await send("<@{}> : **Il faut instancier le bot: voir .Check init**".format(context.author.id),context.channel)
+        raise
+    print(context.author,context.channel)
+    # await context.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+    classe=convert(args[0])
+
+    if got_the_role(liste_eleves[str(context.guild.id)]['admin'],context.author.roles):
+        liste_eleves[str(context.guild.id)]['appels'][str(context.message.id)]={'ClasseRoleID':classe,'listStudents':[]}
+        await send("*Début de l'appel:*\n**Elèves : Cliquez sur ✅ pour vous notifier présent\nProfesseur : Cliquez sur 🆗 pour valider l'appel**",context.channel)
+        await add_reaction("✅",context.message) #on rajoute les réactions ✅ & 🆗
+        await add_reaction("🆗",context.message)
+    else:
+        await send("<@{}> : **Vous n'êtes pas professeur ! Vous ne pouvez démarrer l'appel.**".format(context.author.id),context.channel)
+
+@client.command()
+async def addRole(context,*args):
+    global liste_eleves
+    if got_the_role(liste_eleves[str(context.guild.id)]['admin'],context.author.roles):
+        for i in args:
+            liste_eleves[str(context.guild.id)]['admin'].append(convert(i))
+            await send('*Nouvel admin :*{}'.format(i),context.channel)
+    print(liste_eleves)
+
+
+@client.command()
+async def rmRole(context,*args):
+    global liste_eleves
+    if got_the_role(liste_eleves[str(context.guild.id)]['admin'],context.author.roles):
+        for i in args:
+            del liste_eleves[str(context.guild.id)]['admin'][i]
+            await send('*Admin retiré :*{}'.format(i),context.channel)
+    print(liste_eleves)
+
+@client.command()
+async def init(context,*args):
+    global liste_eleves
+    if len(args)>2:
+        await send("Vous devez mettre deux arguments, le rôle exclusif @ du bot et un rôle admin",context.channel)
+        raise
+    if not str(context.guild.id) in liste_eleves.keys():
+        liste_eleves[str(context.guild.id)]={"botID":convert(args[0]),"admin":[convert(args[1])],"appels":{}}
+        await send("Bot instancié :\n•Bot : <@&{}>\n• Admin : <@&{}>".format(convert(args[0]),convert(args[1])),context.channel)
+    else:
+        await send("Bot déjà défini",context.channel)
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        await send("Commande inconnue :\n"+commandes,ctx.message.channel)
+    raise error
+
+
+
+client.run(token)
+client.add_command(appel)
+client.add_command(addRole)
+client.add_command(rmRole)
